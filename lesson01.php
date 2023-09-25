220b9487210a32743a373354495da0f11345251e
CarlJacob1 <jacobgwapo33@gmail.com>
2023-09-25 20:52:20 +0800

    first commit

---
+++ lesson01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Tutorials</title>
+</head>
+<body>
+    <?php
+     #this is a single a line
+     echo "this comes after a single line comment";
+    /*Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nisi explicabo, 
+    aspernatur omnis sapiente id harum, tempora saepe tempore nihil esse nesciunt 
+    velit optio in hic, molestias voluptatum fugit asperiores?*/
+    
+    echo "<br>this echo comes after a multiline comment";
+?>
+</body>
+</html>
---
+++ lesson02.php
@@ -0,0 +1,6 @@
+<?php
+
+    echo "hello world";
+    echo "this is lesson02";
+    echo "ang saya mag code";
+?>
